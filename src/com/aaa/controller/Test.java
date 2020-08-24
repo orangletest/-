72b5ae410946f33e1b84fb9d7b1600de3c930712
奥特曼 <596877536@qq.com>
2020-08-24 21:38:51 +0800

    修改了

--- src/com/aaa/controller/Test.java
+++ src/com/aaa/controller/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("少年提交了test");
         System.out.println();
+        System.out.println("333");
     }
 }
